5f731ff6d1159cd2c35e053837b22ed6b1b6a783
KaceCottam <kace.cottam@wsu.edu>
2019-04-17 22:14:03 -0700

    Added button Test

--- spike/ButtonTest.cpp
+++ spike/ButtonTest.cpp
@@ -1,9 +1,8 @@
+#include <array>
+#include <stdexcept>
 #include <fmt/format.h>
 #include <spdlog/spdlog.h>
 #include <SFML/Graphics.hpp>
-#include <functional>
-#include <iostream>
-#include <memory>
 
 using namespace sf;
 using namespace fmt;
@@ -11,82 +10,174 @@ using namespace spdlog;
 using namespace fmt::literals;
 
 namespace GUI {
-class Button : public Drawable {
- public:
-  Button(const Transform &area, const Sprite &activated, const Sprite &disabled,
-         const Sprite &selected, RenderWindow *window)
-      : area_{area},
-        activated_{activated},
-        disabled_{disabled},
-        selected_{selected},
-        window_{window},
-        current_sprite_{&activated_} {}
-  void draw(RenderTarget &window,
-            RenderStates states = RenderStates::Default) const override {
-    if (states.transform == RenderStates::Default.transform)
-      states.transform = area_;
-    window.draw(*current_sprite_, states);
+class Clickable {
+ protected:
+  void ParseMouseClick(const Event& event) {
+    if (event.type == Event::MouseButtonPressed &&
+        InArea({(float)event.mouseMove.x, (float)event.mouseMove.y})) {
+      OnClick(event);
+    }
   }
-  void ParseEvent(Event &event) {
-    if (event.type == Event::MouseButtonPressed) {
-      if (event.mouseButton.x >= area_.left &&
-          event.mouseButton.x <= area_.width - area_.left &&
-          event.mouseButton.y >= area_.top &&
-          event.mouseButton.y <= area_.height - area_.top) {
-        // info("Button clicked!");
-        this->OnClick();
+  virtual void OnClick(const Event& event) = 0;
+  Clickable(Window& window)
+      : window_{window} {}
+
+  void SetArea(const FloatRect& area) { area_ = area; }
+
+ private:
+  inline bool InArea(const Vector2f& point) const {
+    if (point.x >= area_.left && point.x <= area_.width + area_.left &&
+        point.y >= area_.top && point.y <= area_.height + area_.top)
+      return true;
+    return false;
+  }
+  FloatRect area_;
+  Window& window_;
+};
+
+class Hoverable {
+ protected:
+  void ParseMouseHover(const Event& event) {
+    static bool in_area_prev{in_area_};
+    if (event.type == Event::MouseMoved &&
+        InArea({(float)event.mouseMove.x, (float)event.mouseMove.y})) {
+      if (in_area_ != in_area_prev) {
+        OnEnter(event);
       }
+      OnHover(event);
+    } else if (in_area_ != in_area_prev) {
+      OnExit(event);
+    }
+
+    in_area_prev = in_area_;
+  }
+  virtual void OnHover(const Event& event) {}
+  virtual void OnEnter(const Event& event) {}
+  virtual void OnExit(const Event & event) {}
+  Hoverable(Window& window)
+      : window_{window} {}
+  void SetArea(const FloatRect& area) { area_ = area; }
+
+ private:
+  inline bool InArea(const Vector2f& point) {
+    if (point.x >= area_.left && point.x <= area_.width + area_.left &&
+        point.y >= area_.top && point.y <= area_.height + area_.top) {
+      in_area_ = true;
+    } else {
+      in_area_ = false;
+    }
+    return in_area_;
+  }
+
+  bool in_area_;
+  FloatRect area_;
+  Window& window_;
+};
+
+class Button : public Clickable, public Hoverable, public Drawable {
+ public:
+  Button(Window &window)
+      : Clickable{window}, Hoverable{window}, current_sprite_{nullptr} { }
+
+  void SetSprite(Sprite* sprite) {
+    current_sprite_ = sprite;
+    SetArea(current_sprite_->getGlobalBounds());
+  }
+
+  void draw(RenderTarget& window,
+            RenderStates states = RenderStates::Default) const override {
+    window.draw(*current_sprite_,states);
+  }
+
+ private:
+  void SetArea(const FloatRect &area) {
+    Clickable::SetArea(area);
+    Hoverable::SetArea(area);
+  }
+  Sprite *current_sprite_;
+};
+
+struct FileNotFoundException : std::exception {
+  FileNotFoundException(const char *filename) {
+    error(format("File \"{}\" not found!",filename));
+  }
+};
+
+inline Sprite makeSpriteFromFile(const char *filename) {
+  Texture tex;
+  if(!tex.loadFromFile(filename)) throw FileNotFoundException(filename);
+  Sprite spr{tex};
+  info(format("Created sprite from file \"{}\".",filename));
+  return spr;
+}
+}  // namespace GUI
+
+template<unsigned long long int Number>
+class ClickyRoundButton : public GUI::Button {
+ public:
+  ClickyRoundButton(RenderWindow &window, const std::array<Sprite, Number>& buttons) 
+  : Button{window}, buttons_{buttons} {
+    SetSprite(&buttons_[increment++ % Number]);
+  }
+
+  void ParseEvent(const Event &event) {
+    ParseMouseClick(event);
+    ParseMouseHover(event);
+  }
+
+  void OnClick(const Event &event) override {
+    SetSprite(&buttons_[increment++ % Number]);
+    info("Clicked!");
+  }
+
+  void OnEnter(const Event &event) override {
+    info("Enter button!");
+  }
+
+  void OnExit(const Event &event) override {
+    info("Exit button!");
+  }
+
+  void OnHover(const Event &event) override {
+    static std::size_t increment{0};
+    if(!(increment++ % 64))
+    {
+      info("Hovering!");
     }
   }
 
- protected:
-  virtual void OnClick() = 0;
-  sf::Transform area_;
-  sf::Sprite activated_;
-  sf::Sprite disabled_;
-  sf::Sprite selected_;
-  sf::RenderWindow *window_;
-  sf::Sprite *current_sprite_;
-};
-}  // namespace GUI
-
-class ChangeColorBtn : public GUI::Button {
- public:
-  ChangeColorBtn(const Transform &area, const Sprite &activated,
-                 const Sprite &disabled, const Sprite &selected,
-                 RenderWindow *window)
-      : Button{area, activated, disabled, selected, window} {}
-
  private:
-  std::size_t increment_;
-  void OnClick() override {
-    current_sprite_ = ((&activated_) + increment_++ % 3);
-  }
+  std::array<Sprite, Number> buttons_;
+  std::size_t increment{0U};
 };
 
 int main() {
   RenderWindow window(VideoMode{800, 800}, "Button Test");
-  Texture red{};
-  Texture blue{};
-  Texture green{};
-  if (!red.loadFromFile("red.jpeg")) info("red not loaded");
-  if (!blue.loadFromFile("blue.jpeg")) info("blue not loaded");
-  if (!green.loadFromFile("bunny.jpeg")) info("green not loaded");
-  std::cout << "test" << '\n';
-  ChangeColorBtn button1{
-      Transform(FloatRect{100, 100, 300, 200}), Sprite{red}, Sprite{blue}, Sprite{green}, &window};
+  Texture button_tex;
+  if(!button_tex.loadFromFile("RoundButton1.png")) throw GUI::FileNotFoundException("RoundButton1.png");
+  Sprite button1{button_tex};
+  Sprite button2{button_tex};
+  Sprite button3{button_tex};
+  button1.setColor(sf::Color::Red);
+  button1.scale({3, 3});
+  button2.setColor(sf::Color(128,128,255));
+  button2.scale({3, 3});
+  button3.setColor(sf::Color::Green);
+  button3.scale({3, 3});
+
+  ClickyRoundButton<3> button{window,{button1,button2,button3}};
+
   while (window.isOpen()) {
     Event event{};
     while (window.pollEvent(event)) {
       if (event.type == Event::Closed) {
         window.close();
       }
-
-      button1.ParseEvent(event);
+      button.ParseEvent(event);
     }
 
     window.clear();
-    window.draw(button1);
+    window.draw(button);
     window.display();
   }
   return EXIT_SUCCESS;
