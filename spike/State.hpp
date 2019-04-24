1c3b4a8f944c3ff36cc5457e7ed4861d74ec525e
KaceCottam <kace.cottam@wsu.edu>
2019-04-23 21:13:52 -0700

    Added spike cxx standard

---
+++ spike/State.hpp
@@ -0,0 +1,14 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <optional>
+#include "Board.h"
+
+class State : public sf::Drawable {
+ public:
+  virtual void HandleEvent(const sf::Event &e) = 0;
+  virtual void draw(sf::RenderTarget &target,
+                    sf::RenderStates states = sf::RenderStates::Default) const = 0;
+  virtual std::optional<int> getNextState() const = 0;
+  virtual bool LoadResources() {}
+  virtual bool UnloadResources() {}
+};
